f169fcb253e78a773c7a53e3a99d244be67edf63
Wallace Randal <wmoura@bionexo.com>
2020-07-28 17:30:04 -0300

    fu

--- src/slides/Slide3.vue
+++ src/slides/Slide3.vue
@@ -4,7 +4,7 @@
             <img src="https://i.imgur.com/hOFChuP.png" 
                  title="source: imgur.com" 
                  class="img-fluid mx-auto d-block rounded m-4"/>
-            <figcaption>É normal não estar bem!</figcaption>
+            <figcaption>É normal não estar bem! :)</figcaption>
         </figure>
     </div>
 </template>
